d96ca810a9a94d057cac3ca481dc646d5354e8c4
DFtorres <dftorres2004@gmail.com>
2024-05-18 00:58:30 -0500

    Global styles created

---
+++ src/views/Styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const GlobalStyles = StyleSheet.create({
+    root: {
+      padding: 20,
+      backgroundColor: "#FFF9D0",
+      top: 0,
+      paddingBottom: 100
+    },
+    title: {
+      padding: 20,
+      fontSize: 40,
+    },
+    listItem: {
+      backgroundColor: "#A0DEFF",
+      padding: 8,
+      margin: 5,
+      borderCurve: "circular",
+      borderRadius: 20,
+    },
+    listTitle: {
+      padding: 8,
+      fontSize: 40
+    },
+    listSubTitle: {
+        padding: 8,
+    },
+    listDescription: {
+        padding: 8,
+    }
+  });
